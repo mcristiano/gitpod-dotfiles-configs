e474f207ee3e4a478d3e5fd496abbf2e5d56b1eb
Chris BR <cristianosistemas@outlook.com>
2022-09-17 18:48:56 -0300

    scripts

---
+++ scritps/install-linux.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt install -y tmux nano mc htop fonts-firacode fonts-powerline
+
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+alias ztheme='(){ export ZSH_THEME="$@" && source $ZSH/oh-my-zsh.sh }'
+
+ztheme bira
+
+

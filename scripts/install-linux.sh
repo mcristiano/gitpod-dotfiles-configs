e3d67bfab5aa129bb85c9c2054fcaebd94519903
Chris BR <cristianosistemas@outlook.com>
2022-09-17 19:55:36 -0300

    ajustes

--- scripts/install-linux.sh
+++ scripts/install-linux.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-sudo apt install -y tmux nano mc htop fonts-firacode fonts-powerline
+sudo apt install -y tmux nano mc htop fonts-firacode fonts-powerline powerline 
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
-alias ztheme='(){ export ZSH_THEME="$@" && source $ZSH/oh-my-zsh.sh }'
+sed -i 's/_THEME=\"robbyrussel\"/_THEME=\"bira\"/g' ~/.zshrc
 
-ztheme bira
+#alias ztheme='(){ export ZSH_THEME="$@" && source $ZSH/oh-my-zsh.sh }'
+#bash "ztheme bira"
 
 
+
+#ZSH_THEME="powerlevel9k/powerlevel9k"
+#POWERLEVEL9K_DISABLE_RPROMPT=true
+#POWERLEVEL9K_PROMPT_ON_NEWLINE=true
+#POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="▶"
+#POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX=""

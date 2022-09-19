68edf892135673dcef21db3bc5b99cd2f220cd0c
mcristiano <36131606+mcristiano@users.noreply.github.com>
2022-09-19 18:26:49 -0300

    Update install-linux.sh

--- scripts/install-linux.sh
+++ scripts/install-linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt install -y tmux nano mc fonts-firacode fonts-powerline powerline 
 
-# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
 
 #ZSH_THEME="powerlevel9k/powerlevel9k"
 #POWERLEVEL9K_DISABLE_RPROMPT=true
